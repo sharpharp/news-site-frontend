1057e9c0364164cceb4ca5ec3698ffac5a56595e
Harper Holden <harp.holden@gmail.com>
2016-03-23 01:17:42 -0500

    finishing touches

--- app.js
+++ app.js
@@ -10,4 +10,31 @@ $(document).ready(function() {
       .closest('.news-item')
       .addClass('is-liked');
   });
+
+// -----------------------------------------
+    // Add Link
+// ------------------------------------------
+
+  $('.js-add-link').on('click', function(event) {
+        event.preventDefault();
+
+        $('.js-form').toggleClass('is-visible')
+      });
+
+// -----------------------------------------
+    // Modal
+// ------------------------------------------
+
+  $('.js-show-modal').on('click', function(event) {
+        event.preventDefault();
+
+        $('.js-modal').addClass('is-visible');
+        $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event) {
+  $('.js-modal').addClass('is-visible');
+        $('.js-modal').removeClass('is-visible');
+        $('.js-modal-overlay').removeClass('is-visible');
+  });
 });
